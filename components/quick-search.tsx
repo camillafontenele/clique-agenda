df0add94f8eb77260fcd2b29456dbfaeaddaa196
Camilla Fontenele <camillafontenele@gmail.com>
2026-04-10 08:51:36 -0300

    feat: enhance QuickSearch component with input and button, update PageSectionScroller styles, and add custom type declarations

--- components/quick-search.tsx
+++ components/quick-search.tsx
@@ -1,57 +1,78 @@
-import { Eye, Footprints, Scissors, Sparkles, User } from "lucide-react";
+import {
+  Eye,
+  Footprints,
+  Scissors,
+  SearchIcon,
+  Sparkles,
+  User,
+} from "lucide-react";
 import Link from "next/link";
 
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { PageSectionScroller } from "@/components/ui/page";
 
 const QuickSearch = () => {
   return (
-    <PageSectionScroller>
-      <Link
-        href="/barbershops?search=cabelo"
-        className="border-border bg-muted flex shrink-0 items-center justify-center gap-3 rounded-3xl px-4 py-2"
-      >
-        <Scissors className="size-4" />
-        <span className="text-card-foreground text-sm font-medium">Cabelo</span>
-      </Link>
+    <>
+      <div className="flex items-center gap-2">
+        <Input className="border-border rounded-full" placeholder="Pesquisar" />
+        <Button className="h-10 w-10 rounded-full">
+          <SearchIcon />
+        </Button>
+      </div>
+      <PageSectionScroller>
+        <Link
+          href="/barbershops?search=cabelo"
+          className="border-border bg-muted flex shrink-0 items-center justify-center gap-3 rounded-3xl px-4 py-2"
+        >
+          <Scissors className="size-4" />
+          <span className="text-card-foreground text-sm font-medium">
+            Cabelo
+          </span>
+        </Link>
 
-      <Link
-        href="/barbershops?search=barba"
-        className="border-border bg-muted flex shrink-0 items-center justify-center gap-3 rounded-3xl px-4 py-2"
-      >
-        <User className="size-4" />
-        <span className="text-card-foreground text-sm font-medium">Barba</span>
-      </Link>
+        <Link
+          href="/barbershops?search=barba"
+          className="border-border bg-muted flex shrink-0 items-center justify-center gap-3 rounded-3xl px-4 py-2"
+        >
+          <User className="size-4" />
+          <span className="text-card-foreground text-sm font-medium">
+            Barba
+          </span>
+        </Link>
 
-      <Link
-        href="/barbershops?search=acabamento"
-        className="border-border bg-muted flex shrink-0 items-center justify-center gap-3 rounded-3xl px-4 py-2"
-      >
-        <Sparkles className="size-4" />
-        <span className="text-card-foreground text-sm font-medium">
-          Acabamento
-        </span>
-      </Link>
+        <Link
+          href="/barbershops?search=acabamento"
+          className="border-border bg-muted flex shrink-0 items-center justify-center gap-3 rounded-3xl px-4 py-2"
+        >
+          <Sparkles className="size-4" />
+          <span className="text-card-foreground text-sm font-medium">
+            Acabamento
+          </span>
+        </Link>
 
-      <Link
-        href="/barbershops?search=sobrancelha"
-        className="border-border bg-muted flex shrink-0 items-center justify-center gap-3 rounded-3xl px-4 py-2"
-      >
-        <Eye className="size-4" />
-        <span className="text-card-foreground text-sm font-medium">
-          Sobrancelha
-        </span>
-      </Link>
+        <Link
+          href="/barbershops?search=sobrancelha"
+          className="border-border bg-muted flex shrink-0 items-center justify-center gap-3 rounded-3xl px-4 py-2"
+        >
+          <Eye className="size-4" />
+          <span className="text-card-foreground text-sm font-medium">
+            Sobrancelha
+          </span>
+        </Link>
 
-      <Link
-        href="/barbershops?search=pezinho"
-        className="border-border bg-muted flex shrink-0 items-center justify-center gap-3 rounded-3xl px-4 py-2"
-      >
-        <Footprints className="size-4" />
-        <span className="text-card-foreground text-sm font-medium">
-          Pézinho
-        </span>
-      </Link>
-    </PageSectionScroller>
+        <Link
+          href="/barbershops?search=pezinho"
+          className="border-border bg-muted flex shrink-0 items-center justify-center gap-3 rounded-3xl px-4 py-2"
+        >
+          <Footprints className="size-4" />
+          <span className="text-card-foreground text-sm font-medium">
+            Pézinho
+          </span>
+        </Link>
+      </PageSectionScroller>
+    </>
   );
 };
 
